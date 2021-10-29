743e1678d949bbe11ee9ed7e41133bf97b04f09a
anonymous anonymous <jelani@firstdraft.com>
2021-10-29 17:23:36 +0000

    Whitelist columns in Attendance for ActiveAdmin

--- app/admin/attendances.rb
+++ app/admin/attendances.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Attendance do
 
+ permit_params :attending, :user_id, :event_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
