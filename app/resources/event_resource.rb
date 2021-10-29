503315d85f8d57891c2872e49020a3d59277eb62
anonymous anonymous <jelani@firstdraft.com>
2021-10-29 17:24:09 +0000

    Generate graphiti resources for events

---
+++ app/resources/event_resource.rb
@@ -0,0 +1,14 @@
+class EventResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :starts_at, :datetime
+  attribute :ends_at, :datetime
+  attribute :user_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
