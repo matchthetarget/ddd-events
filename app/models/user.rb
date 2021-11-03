c74213eb0e44b9e6dc2c9347b66e6c909a907e3e
Jelani Woods <jelani@firstdraft.com>
2021-11-03 11:57:09 -0500

    Event show done

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,9 @@ class User < ApplicationRecord
     first_name
   end
 
+  def full_name
+    first_name + " " + last_name
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
